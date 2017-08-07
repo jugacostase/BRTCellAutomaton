16753eebee08b715576962f120ffa66e0a369fea
Juan Guillermo Acosta <jugacostase@unal.edu.co>
2017-08-07 14:26:34 -0500

    the template cpp file to  begin the NaSch model program has been created

---
+++ OneLane_NaSch.cpp
@@ -0,0 +1,18 @@
+// Malla de Trafico
+#include <iostream>
+#include<fstream>
+#include<cstdlib>
+#include <cmath>
+#include <cstdlib> 
+#include <ctime> 
+#include <vector>
+#include "Random64.h"
+using namespace std;
+
+
+
+int main(void){
+
+
+  return 0;
+}
